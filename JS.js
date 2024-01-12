531d5959bc6ec3ee5f7f75586777e99a25a9c82a
Krzysztof Augustyniak <office@pkproject.pl>
2024-01-12 12:36:06 +0100

    building html&js structure

---
+++ JS.js
@@ -0,0 +1,27 @@
+/* product quantity */
+let quantityProduct = 1;
+
+/* Products */
+let product1 = {
+    product: "Piłka nożna",
+    type: "outside",
+    price: 98.00,
+    picture: 'src="./Images/FootballBall.jpg"'
+};
+
+/* inputs */
+let productList = document.getElementById("Products");
+
+/* get element */
+let wszystko = document.querySelector(".wszystko");
+
+/* add listner */
+wszystko.addEventListener("click", wszystko())
+
+/* Function */
+function wszystko() {
+    if (i=0, i<quantityProduct) {
+        i++ ;
+        
+    }
+}
