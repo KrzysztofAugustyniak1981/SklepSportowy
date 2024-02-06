4d0f5d391704d59991a5cdcdde8247cceefbc362
Krzysztof Augustyniak <office@pkproject.pl>
2024-02-06 14:03:44 +0100

    to cleaninig code

--- JS.js
+++ JS.js
@@ -1,21 +1,25 @@
 
 /* Products */
 let product = [
-    ["Piłka nożna","outside",98.00,"FootballBall.jpg"],
-    ["Piłka do koszykówki","outside",89.00,"BasketBall.jpg"],
-    ["Deskorolka","outside",151.00,"Deskorolka.jpg"],
-    ["Hankle ciężkie","inside",122.99,"Hankle.jpg"],
-    ["Hankle lekkie","inside",69.99,"HankleLekkie.jpg"],
-    ["Skuter wodny","water",3698.00,"JetSki.jpg"],
-    ["Kamizelka Ratunkowa","water",139.00,"KamizelkaRatunkowa.jpg"],
-    ["Kajak","water",870.00,"Kayak.jpg"],
-    ["Łuk sportowy","outside",240.00,"Łuk.jpg"],
-    ["Maska z płetwami","water",695.00,"MaskaPłetwy.jpg"],
+    ["Piłka nożna","outside",98.00,"FootballBall.jpg",0],
+    ["Piłka do koszykówki","outside",89.00,"BasketBall.jpg",0],
+    ["Deskorolka","outside",151.00,"Deskorolka.jpg",0],
+    ["Hankle ciężkie","inside",122.99,"Hankle.jpg",0],
+    ["Hankle lekkie","inside",69.99,"HankleLekkie.jpg",0],
+    ["Skuter wodny","water",3698.00,"JetSki.jpg",0],
+    ["Kamizelka Ratunkowa","water",139.00,"KamizelkaRatunkowa.jpg",0],
+    ["Kajak","water",870.00,"Kayak.jpg",0],
+    ["Łuk sportowy","outside",240.00,"Łuk.jpg",0],
+    ["Maska z płetwami","water",695.00,"MaskaPłetwy.jpg",0],
     
 ];
 let length = product.length;
+let cartPrice = 0;
+let cartPriceRe = 0;
 /* inputs */
 let productList = document.getElementById("Products");
+let cartPlace = document.getElementById("cart");
+let cartOverlay = document.getElementById("cartView");
 
 
 /* get element */
@@ -137,6 +141,31 @@ function NaDworzeInput() {
     document.getElementById("naDworze").classList.add('active');
 }
 
+const buttonGroup = document.getElementById("Products");
+let buttonGroupPressed = e => {
+    let isButton = e.target.nodeName === 'BUTTON';
+    if(!isButton) {
+        return
+    } let buying = e.target.id;
+    let buyingProduct = buying.replace(/\D/g, "");
+    if (buying.length < 11) {
+        let changeProdukt =  product[buyingProduct][4];
+        if (changeProdukt>0) {
+            product[buyingProduct][4]--;
+            console.log(product[buyingProduct][4]);
+        }
+        
+    } else if (buying.length > 10) {
+        
+        product[buyingProduct][4]++;
+        console.log(product[buyingProduct][4]);
+    } cartRefresh()
+}
+buttonGroup.addEventListener("click", buttonGroupPressed);
+cartOverlay.addEventListener("click", buttonGroupPressed);
+cartOverlay.addEventListener("click", openModal);
+
+
 
 function clear() {
     productList.innerHTML = "";
@@ -148,6 +177,43 @@ function clear() {
     document.getElementById("doWody").classList.remove('active');
     if ( document.getElementById("naDworze").classList.contains('active'))
     document.getElementById("naDworze").classList.remove('active');
+}
+function cartRefresh() {
+    let i = 0;
+    cartPrice = 0;
+    fresh();
+    function fresh() {
+    if (length>i) {
+        cartPriceRe = product[i][2] * product[i][4];
+        cartPrice += cartPriceRe;
+        i++;
+        fresh();
+    }
+    cartPlace.innerHTML = cartPrice + "Pln";
+};}
+
+function openModal() {
+    document.getElementById('overlay').style.display = 'flex';
+    cartOverlay.innerHTML = '';
+    let i = 0;
+    let productInCart = 0;
+    while (i < length) {
+        if (product[i][4]>0) {
+            productInCart = product[i][4] * product[i][2];
+            cartOverlay.innerHTML +=
+            '<div class="text-center row"><div class="my-1 col-3"><span class="fw-bold">' + product[i][0] +
+            '</span></div><div class="my-1 col-2">' + product[i][2] + 
+            '</div><div class="my-1 col-2"><button id="addProduct' + i + '">+</button> x ' + product[i][4] +
+            ' szt. <button id="deduct' + i + '">-</button></div><div class="my-1 col-1">razem:</div><div class="my-1 col-1">' + productInCart + 'Pln</div><br></div>'
+        }
+        i++;
+    }
+    cartOverlay.innerHTML += '<div class="text-end fw-bold">Kwota do zapłaty : ' + cartPrice + '</div>'
+};
+
+function closeModal() {
+    document.getElementById('overlay').style.display = 'none';
+
 }
 
 
